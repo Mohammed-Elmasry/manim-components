baaebfc295e780d89c746f806ccb88026c9c5d5c
Mohammed-Elmasry <38670846+Mohammed-Elmasry@users.noreply.github.com>
2023-04-03 13:13:06 +0200

    implement code snippet

--- code_snippet.py
+++ code_snippet.py
@@ -1,44 +1,60 @@
 from manim import *
+from manim.mobject.text import tex_mobject
 
 
 class CodeSnippetExample(Scene):
     def construct(self):
-        lines_of_code = 0.25 + 2
-        function_signature = Tex(r"public ", r"static ", r"void ", r"setName() \{")
-        # function_signature = Tex(r"while ", r"(x ", "> ", r"1) \{")
-        function_signature.move_to(UP)
+        # lines_of_code = 0.25 + 2
+        # function_signature = Tex(r"public ", r"static ", r"void ", r"setName() \{")
+        #
+        # function_body_line1 = Tex("this.name = name;")
+        # function_body_line2 = Tex("this.sample = sample;")
+        #
+        # function_close = Tex(r"\}")
+        #
+        # function_body_line1.next_to(function_signature, DOWN)
+        # function_body_line2.next_to(function_body_line1, DOWN, aligned_edge=LEFT)
+        # function_close.move_to(function_signature.get_left() + DOWN * lines_of_code)  # TODO investigate line of code
 
-        function_body_line1 = Tex("this.name = name;")
-        function_body_line2 = Tex("this.sample = sample;")
+        # self.wait(1)
+        #
+        # function_snippet = VGroup(function_signature, function_body_line1, function_body_line2,
+        #                           function_close)
+        #
+        # self.play(Write(function_snippet))
+        # self.wait()
+        #
+        # self.play(function_snippet.animate().scale(0.7).to_edge(UL), run_time=2)
+        #
+        # self.wait(1)
+        # added_lines = 2
+        #
+        # function_body_line3 = Tex("var minimum = Math.min(2, 3);").scale(0.7)
+        # function_body_line4 = Tex("var maximum = Math.max(2, 3);").scale(0.7)
+        # function_body_line3.next_to(function_body_line2, DOWN, aligned_edge=LEFT)
+        # function_body_line4.next_to(function_body_line3, DOWN, aligned_edge=LEFT)
+        #
+        # self.wait()
+        # function_snippet.add(function_body_line3, function_body_line4)
+        #
+        # self.play(function_snippet[3].animate().shift(DOWN * 0.5 * added_lines), Write(function_body_line3),
+        #           Write(function_body_line4))
+        #
+        # self.wait()
+        #
+        # while_sig = Tex(r"while (x == 1) ", r"\{")
+        # while_close = Tex(r"\}")
+        #
+        # self.play(function_close.animate().shift(DOWN * 0.5 * 4))
+        #
+        # while_sig.next_to(function_close, UR, aligned_edge=LEFT).scale(0.7)
+        # while_close.next_to(while_sig, DOWN, aligned_edge=LEFT).scale(0.7)
+        #
+        # function_snippet.add(while_sig, while_close)
+        #
+        # self.play(Write(while_sig), Write(while_close))
+        #
+        # self.wait()
 
-        function_close = Tex(r"\}")
-
-        function_body_line1.next_to(function_signature, DOWN)
-        function_body_line2.next_to(function_body_line1, DOWN, aligned_edge=LEFT)
-        function_close.move_to(function_signature.get_left() + DOWN * lines_of_code)
-
-        self.play(Write(function_signature))
-        self.play(Write(function_body_line1))
-        self.play(Write(function_body_line2))
-        self.play(Write(function_close))
-
-        self.wait(1)
-
-        code_snippet = VGroup(function_signature, function_body_line1, function_body_line2,
-                              function_close)
-
-        self.play(code_snippet.animate().scale(0.7).to_edge(UL), run_time=2)
-
-        self.wait(1)
-
-        function_body_line3 = Tex("var result = Math.min(2, 3);").scale(0.7)
-        function_body_line3.next_to(function_body_line2, DOWN, aligned_edge=LEFT)
-        self.play(code_snippet[3].animate().shift(DOWN), run_time=1)
-        self.play(Write(function_body_line3))
-        code_snippet.add(function_body_line3)
-
-        self.wait(1)
-
-        self.play(code_snippet.animate().to_edge(UR))
-
-        self.wait(1)
+        plane = NumberPlane()
+        self.add(plane)
